77cd0718ae330ea19dbec7aae65a34ad104ed0de
CPKVG <51846930+CPKVG@users.noreply.github.com>
2022-01-27 18:13:21 +1300

    product result test

--- components/product/productResult/spec.tsx
+++ components/product/productResult/spec.tsx
@@ -1,7 +1,12 @@
-import React from "react"
+import React from "react";
 import { cleanup, fireEvent } from "@testing-library/react-native";
 import ProductResult from ".";
 import { NavigationContainer } from '@react-navigation/native';
+import renderer from "react-test-renderer";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import ProductImage from "../productImage";
+import { render, store } from "../../../test-utils";
+import { shallow } from "enzyme";
 
 jest.useFakeTimers()
 
@@ -14,9 +19,93 @@ describe('productResult component', () => {
         </NavigationContainer>
     )
 
+    const component1 = (
+      <View>
+        <Text testID="titleID">
+        titleID
+        </Text>
+      </View>
+    )
+      
+    const productDataSearch = [{
+        category:"Apparel",
+        description:"Keeps you warm",
+        id:"0",
+        image:"White_t-shirt",
+        price:"5.99",        
+        stock:"2",
+        title:"White T-shirt",
+    }]
+
     test('should render', () => { 
         expect(component).not.toBe(null)
       })
+
+
+      //TODO, testing flatlist on react-native seems too difficult
+
+      // it('should flatlist return keyExtractor correctly', () => {
+      //   const renderItem = jest.fn();
+      //   const { getByTestId  } = render(component, {    
+      //     productDataSearch
+      //     })
+      //     fireEvent.press(getByTestId('flat-list'));
+      //     expect(renderItem).toHaveBeenCalled();
+       
+      //  });
+
+
+      // it("should render product search results",()=>{
+      //   const { getByText } = render(component, {    
+      //     productDataSearch
+      //     })
+      //     expect(getByText("White T-shirt")).toBeTruthy();
+      // })
+
+    // it("should render product search results",()=>{
+    //     const { getByTestId } = render(component, {    
+    //       productDataSearch
+    //       })
+    //       // expect(getByTestId("imageID")).toBeTruthy();
+    //       expect(getByTestId("titleID")).toBeTruthy();
+    //       // expect(getByTestId("priceID")).toBeTruthy();
+    // })
     
+
+      
+  it("renders the productDetail component via snapshot",()=> {
+    const renderItem = ({item}:any) => {
+    
+        return(
+          //<ProductImage/>
+          <TouchableOpacity>
+            <Image 
+              source = {ProductImage(item.image)}
+            />
+            <Text>
+              {item.title}
+            </Text>
+            <Text>
+              ${item.price} nzd
+            </Text>
+          </TouchableOpacity >
+        )
+      }
+    const tree = renderer.create(
+        <View>
+        <FlatList  
+        data={productDataSearch}
+        numColumns ={2}
+        ItemSeparatorComponent={
+            () => <View style={{ width: 16 }}/>
+        }
+        renderItem={renderItem}
+  
+    />
+  
+      </View>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 
 })
